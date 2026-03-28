0754ea83b7252a816a3f2f9f89c97a7742540602
RianCatawan <catawanrian054@gmail.com>
2026-03-29 01:24:44 +0800

    link navigations

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$server_path = __DIR__ . "/../uploads/" . $file;
+include(__DIR__ . '/../database&config/db.php');
 
 $current_user = $_SESSION['username'] ?? '';
 $base = "";
