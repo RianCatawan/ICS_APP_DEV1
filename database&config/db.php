6950a0302f33bd85587d88ecf9d079368870b1af
RianCatawan <catawanrian054@gmail.com>
2026-03-28 21:49:06 +0800

    link navigations

--- database&config/db.php
+++ database&config/db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "basketball_matchmaker";
+$dbname = "university_hoops1";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
