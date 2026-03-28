df44a3ee6b7c234f366d1bd75356a7411af36a7f
RianCatawan <catawanrian054@gmail.com>
2026-03-29 07:32:39 +0800

    added

--- authentication/login.php
+++ authentication/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include(__DIR__ . '/../database_config/db.php');
+include(__DIR__ . '/database_config/db.php');
 
 if(isset($_POST['login'])){
     $username = $_POST['username'];
--- authentication/logout.php
+++ authentication/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include(__DIR__ . '/../database_config/db.php');
+include(__DIR__ . '/database_config/db.php');
 
 // Check if user is logged in
 if (isset($_SESSION['username'])) {
