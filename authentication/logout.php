6ebb78f347363e78c1017408d029624461b188e0
RianCatawan <catawanrian054@gmail.com>
2026-05-02 23:35:52 +0800

    fixing navigation

--- authentication/logout.php
+++ authentication/logout.php
@@ -35,6 +35,6 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 // THE FIX: Redirect specifically to the basketball folder index
-header("Location: ../ICS_APP_DEV1/index.php");
+header("Location: ../index.php");
 exit();
 ?>
